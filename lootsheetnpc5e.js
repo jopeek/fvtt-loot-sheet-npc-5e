9aaaf38bec7cbd4f3809807aa0c50d876b7ff406
arkaris <myachkovd@mail.ru>
2020-05-18 21:20:26 +0300

    fix sheet import

--- lootsheetnpc5e.js
+++ lootsheetnpc5e.js
@@ -1,6 +1,4 @@
-import {
-    ActorSheet5eNPC
-} from "../../systems/dnd5e/module/actor/sheets/npc.js";
+import ActorSheet5eNPC from "../../systems/dnd5e/module/actor/sheets/npc.js";
 
 class LootSheet5eNPC extends ActorSheet5eNPC {
 
