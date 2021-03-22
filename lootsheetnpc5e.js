739c34db19215139d92f6a71814827b996eba7b5
Jan Ole Peek <jopeek@gmail.com>
2021-03-22 08:41:56 -0700

    Update lootsheetnpc5e.js

--- lootsheetnpc5e.js
+++ lootsheetnpc5e.js
@@ -1261,6 +1261,8 @@ Hooks.once("init", () => {
         scope: "world",
         config: true,
         default: false,
+        type: Boolean
+    });
 
     game.settings.register("lootsheetnpc5e", "reduceUpdateVerbosity", {
         name: "Reduce Update Shop Verbosity",
