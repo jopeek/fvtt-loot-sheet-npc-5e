75ae4fe73c674fcf2b7ffb5426993fdb3fe1cc9b
kevin brammer <kevin.brammer@nanotemper.de>
2021-04-24 00:07:00 +0200

    Added an option to raise or lower maximum price modifier to allow more flexibity

--- lootsheetnpc5e.js
+++ lootsheetnpc5e.js
@@ -763,9 +763,11 @@ class LootSheet5eNPC extends ActorSheet5eNPC {
 
         priceModifier = Math.round(priceModifier * 100);
 
+        const maxModifier = game.settings.get("lootsheetnpc5e", "maxPriceIncrease");
+
         var html = "<p>Use this slider to increase or decrease the price of all items in this inventory. <i class='fa fa-question-circle' title='This uses a percentage factor where 100% is the current price, 0% is 0, and 200% is double the price.'></i></p>";
-        html += '<p><input name="price-modifier-percent" id="price-modifier-percent" type="range" min="0" max="200" value="' + priceModifier + '" class="slider"></p>';
-        html += '<p><label>Percentage:</label> <input type=number min="0" max="200" value="' + priceModifier + '" id="price-modifier-percent-display"></p>';
+        html += '<p><input name="price-modifier-percent" id="price-modifier-percent" type="range" min="0" max="' + maxModifier + '" value="' + priceModifier + '" class="slider"></p>';
+        html += '<p><label>Percentage:</label> <input type=number min="0" max="' + maxModifier + '" value="' + priceModifier + '" id="price-modifier-percent-display"></p>';
         html += '<script>var pmSlider = document.getElementById("price-modifier-percent"); var pmDisplay = document.getElementById("price-modifier-percent-display"); pmDisplay.value = pmSlider.value; pmSlider.oninput = function() { pmDisplay.value = this.value; }; pmDisplay.oninput = function() { pmSlider.value = this.value; };</script>';
 
         let d = new Dialog({
@@ -1250,6 +1252,15 @@ Hooks.once("init", () => {
         config: true,
         default: true,
         type: Boolean
+    });
+
+    game.settings.register("lootsheetnpc5e", "maxPriceIncrease", {
+        name: "Maximum Price Increase",
+        hint: "Change the maximum price increase for a merchant in percent",
+        scope: "world",
+        config: true,
+        default: 200,
+        type: Number
     });
 
     function chatMessage(speaker, owner, message, item) {
