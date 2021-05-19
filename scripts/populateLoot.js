a62cd3d0a75757af75aa8303d9d9ccc18e1fc6bc
Daniel Böttner <jackprince1983@gmail.com>
2021-05-19 16:17:30 +0200

    Update populateLoot.js

--- scripts/populateLoot.js
+++ scripts/populateLoot.js
@@ -1,25 +1,19 @@
 import ActorSheet5eNPC from "../../../systems/dnd5e/module/actor/sheets/npc.js";
 import Item5e from "../../../systems/dnd5e/module/item/entity.js";
 
-export default class populateLoot {
+export class populateLoot {
 	constructor() {
 		return this;
 	}
 
-	async generateLoot(scene, data) {
+	static async generateLoot(scene, data) {
         //instead of the main actor we want/need the actor of the token.
-	const tokenId = data._id;
+				const tokenId = data._id;
         const token = canvas.tokens.get(tokenId);
-        const actor = token.actor;	
-    
+        const actor = token.actor;
+
         const moduleNamespace = "lootsheetnpc5e";
-		
-	if(actor.getFlag(moduleNamespace, "rolltable")){
-		const rolltableName = actor.getFlag(moduleNamespace, "rolltable");
-	} else if (game.settings.get("lootsheetnpc5e","fallbackRolltable")){
-		const rolltableName = game.settings.get("lootsheetnpc5e","fallbackRolltable");
-	}
-		
+				const rolltableName = actor.getFlag(moduleNamespace, "rolltable") || game.settings.get("lootsheetnpc5e","fallbackRolltable");
         const shopQtyFormula = actor.getFlag(moduleNamespace, "shopQty") || "1";
         const itemQtyFormula = actor.getFlag(moduleNamespace, "itemQty") || "1";
         const itemQtyLimit = actor.getFlag(moduleNamespace, "itemQtyLimit") || "0";
@@ -27,19 +21,19 @@ export default class populateLoot {
         const itemOnlyOnce = actor.getFlag(moduleNamespace, "itemOnlyOnce");
         const reducedVerbosity = game.settings.get("lootsheetnpc5e", "reduceUpdateVerbosity");
         let shopQtyRoll = new Roll(shopQtyFormula);
-        
-        shopQtyRoll.roll();	
-		
-        if (!rolltableName) {		
+
+        shopQtyRoll.roll();
+
+        if (!rolltableName) {
           return;
         }
 
 	let rolltable = game.tables.getName(rolltableName);
-    
+
         if (!rolltable) {
             return ui.notifications.error(`No Rollable Table found with name "${rolltableName}".`);
         }
-        
+
         if (itemOnlyOnce) {
             if (rolltable.results.length < shopQtyRoll.total)  {
                 return ui.notifications.error(`Cannot create a merchant with ${shopQtyRoll.total} unqiue entries if the rolltable only contains ${rolltable.results.length} items`);
@@ -55,29 +49,29 @@ export default class populateLoot {
             for (let i = 0; i < shopQtyRoll.total; i++) {
                 const rollResult = rolltable.roll();
                 let newItem = null;
-    
+
                 if (rollResult.results[0].collection === "Item") {
                     newItem = game.items.get(rollResult.results[0].resultId);
                 } else {
                     const items = game.packs.get(rollResult.results[0].collection);
                     newItem = await items.getEntity(rollResult.results[0].resultId);
                 }
-              
+
                 if (!newItem || newItem === null) {
                     return;
                 }
-                
+
                 if (newItem.type === "spell") {
                     newItem = await Item5e.createScrollFromSpell(newItem)
                 }
 
                 let itemQtyRoll = new Roll(itemQtyFormula);
                 itemQtyRoll.roll();
-                
+
                 console.log(`Loot Sheet | Adding ${itemQtyRoll.total} x ${newItem.name}`)
-    
+
                 let existingItem = actor.items.find(item => item.data.name == newItem.name);
-       
+
                 if (existingItem === null) {
                     await actor.createEmbeddedEntity("OwnedItem", newItem);
                     console.log(`Loot Sheet | ${newItem.name} does not exist.`);
@@ -92,16 +86,16 @@ export default class populateLoot {
                     }
                 } else {
                     console.log(`Loot Sheet | Item ${newItem.name} exists.`);
-                    
+
                     let newQty = Number(existingItem.data.data.quantity) + Number(itemQtyRoll.total);
-        
+
                     if (itemQtyLimit > 0 && Number(itemQtyLimit) === Number(existingItem.data.data.quantity)) {
                       if (!reducedVerbosity) ui.notifications.info(`${newItem.name} already at maximum quantity (${itemQtyLimit}).`);
                     }
                     else if (itemQtyLimit > 0 && Number(itemQtyLimit) < Number(newQty)) {
                     //console.log("Exceeds existing quantity, limiting");
                       await existingItem.update({ "data.quantity": itemQtyLimit });
-                    
+
                       if (!reducedVerbosity) ui.notifications.info(`Added additional quantity to ${newItem.name} to the specified maximum of ${itemQtyLimit}.`);
                     } else {
                       await existingItem.update({ "data.quantity": newQty });
@@ -119,19 +113,19 @@ export default class populateLoot {
                 let numberOfEntries = rolltable.data.results[index].weight
                 for (let i = 0; i < numberOfEntries; i++) {
                     rolltableIndexes.push(index);
-                }     
+                }
             }
-            
+
             // Shuffle the list of indexes
             var currentIndex = rolltableIndexes.length, temporaryValue, randomIndex;
-      
+
             // While there remain elements to shuffle...
             while (0 !== currentIndex) {
-        
+
                 // Pick a remaining element...
                 randomIndex = Math.floor(Math.random() * currentIndex);
                 currentIndex -= 1;
-            
+
                 // And swap it with the current element.
                 temporaryValue = rolltableIndexes[currentIndex];
                 rolltableIndexes[currentIndex] = rolltableIndexes[randomIndex];
@@ -149,7 +143,7 @@ export default class populateLoot {
                 let usedEntries = rolltableIndexes.slice(0, shopQtyRoll.total + numberOfAdditionalItems);
                 // console.log(`Distinct: ${usedEntries}`);
                 let distinctEntris = [...new Set(usedEntries)];
-                
+
                 if (distinctEntris.length < shopQtyRoll.total) {
                     numberOfAdditionalItems++;
                     // console.log(`numberOfAdditionalItems: ${numberOfAdditionalItems}`);
@@ -160,7 +154,7 @@ export default class populateLoot {
                 // console.log(`indexesToUse: ${indexesToUse}`)
                 break;
             }
-      
+
             for (const index of indexesToUse)
             {
                 let itemQtyRoll = new Roll(itemQtyFormula);
