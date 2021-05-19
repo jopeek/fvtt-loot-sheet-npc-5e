3e05e6f85e0184950cb86f4150f3de4d02de16e3
Daniel Böttner <jackprince1983@gmail.com>
2021-05-19 13:17:45 +0200

    Update onCreateToken.js
    Moved hook handling to this file. Left only population logic in extra file.

--- hooks/onCreateToken.js
+++ hooks/onCreateToken.js
@@ -1,3 +1,17 @@
 import populateLoot from '../scripts/populateLoot.js';
 
-let populateLootHook = new populateLoot;
+Hooks.on('ready', async () => {
+    _hookPreTokenCreate();
+});
+
+function _hookPreTokenCreate() {
+  Hooks.on('createToken', (scene, data, options, userId) => {
+    const actor = game.actors.get(data.actorId);
+
+    if (!actor || (data.actorLink)) // Don't for linked token
+      return data;
+
+    let populateLootHook = new populateLoot;
+    populateLootHook.generateLoot(scene, data);
+  });
+}
