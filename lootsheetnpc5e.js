291a28cbf88d224afaa03de98cfc2bbb30beca05
RubberDuckieGod <92833545+RubberDuckieGod@users.noreply.github.com>
2021-10-25 18:15:19 -0500

    change v1
    change 542-608 to below. moved 583-585 to in front of 574. commented out prior code. new code underneath commented out code. I know nothing about java and all i did was look at differences in code. I apologize before hand just trying to get this to work so as not to be another person who wants changes or fixes but doesn't at least try to help. 
    
        /**
         * Handle buy item
         * @private
         */
        _buyItem(event, all = 0) {
            event.preventDefault();
            console.log("Loot Sheet | Buy Item clicked");
    
            let targetGm = null;
            game.users.forEach((u) => {
                if (u.isGM && u.active && u.viewedScene === game.user.viewedScene) {
                    targetGm = u;
                }
            });
    
            if (!targetGm) {
                return ui.notifications.error("No active GM on your scene, they must be online and on the same scene to purchase an item.");
            }
    
            if (this.token === null) {
                return ui.notifications.error(`You must purchase items from a token.`);
            }
            if (!game.user.actorId) {
                console.log("Loot Sheet | No active character for user");
                return ui.notifications.error(`No active character for user.`);
            }
    
            //let itemId = $(event.currentTarget).parents(".item").attr("data-item-id");
            //const item = this.actor.getEmbeddedEntity("Item", itemId);
    	const itemId = $(event.currentTarget).parents(".item").attr("data-item-id");
    	const targetItem = this.actor.getEmbeddedEntity("Item", itemId);
    
     	if (all || event.shiftKey) {
                item.quantity = targetItem.data.data.quantity;
            }
    
            const packet = {
                type: "buy",
                buyerId: game.user.actorId,
                tokenId: this.token.id,
                //itemId: itemId,
    	    items: [item],
                quantity: 1,
                processorId: targetGm.id
            };
    
            //if (item.data.data.quantity === packet.quantity) {
    	if (targetItem.data.data.quantity === item.quantity) {
                console.log("LootSheet5e", "Sending buy request to " + targetGm.name, packet);
                game.socket.emit(LootSheet5eNPC.SOCKET, packet);
                return;
            }
    
            //let d = new QuantityDialog((quantity) => {
    	const d = new QuantityDialog((quantity) => {
                packet.quantity = quantity;
                console.log("LootSheet5e", "Sending buy request to " + targetGm.name, packet);
                game.socket.emit(LootSheet5eNPC.SOCKET, packet);
            },
                {
                    acceptLabel: "Purchase"
                }
            );
            d.render(true);
        }
    
        /* -------------------------------------------- */

--- lootsheetnpc5e.js
+++ lootsheetnpc5e.js
@@ -538,10 +538,10 @@ class LootSheet5eNPC extends ActorSheet5eNPC {
         await currentActor.setFlag("lootsheetnpc5e", "lootsheettype", selectedItem);
 
     }
+    
+   /* -------------------------------------------- */
 
-    /* -------------------------------------------- */
-
-    /**
+        /**
      * Handle buy item
      * @private
      */
@@ -568,29 +568,34 @@ class LootSheet5eNPC extends ActorSheet5eNPC {
             return ui.notifications.error(`No active character for user.`);
         }
 
-        let itemId = $(event.currentTarget).parents(".item").attr("data-item-id");
-        const item = this.actor.getEmbeddedEntity("Item", itemId);
+        //let itemId = $(event.currentTarget).parents(".item").attr("data-item-id");
+        //const item = this.actor.getEmbeddedEntity("Item", itemId);
+	const itemId = $(event.currentTarget).parents(".item").attr("data-item-id");
+	const targetItem = this.actor.getEmbeddedEntity("Item", itemId);
+
+ 	if (all || event.shiftKey) {
+            item.quantity = targetItem.data.data.quantity;
+        }
 
         const packet = {
             type: "buy",
             buyerId: game.user.actorId,
             tokenId: this.token.id,
-            itemId: itemId,
+            //itemId: itemId,
+	    items: [item],
             quantity: 1,
             processorId: targetGm.id
         };
 
-        if (all || event.shiftKey) {
-            packet.quantity = item.data.data.quantity;
-        }
-
-        if (item.data.data.quantity === packet.quantity) {
+        //if (item.data.data.quantity === packet.quantity) {
+	if (targetItem.data.data.quantity === item.quantity) {
             console.log("LootSheet5e", "Sending buy request to " + targetGm.name, packet);
             game.socket.emit(LootSheet5eNPC.SOCKET, packet);
             return;
         }
 
-        let d = new QuantityDialog((quantity) => {
+        //let d = new QuantityDialog((quantity) => {
+	const d = new QuantityDialog((quantity) => {
             packet.quantity = quantity;
             console.log("LootSheet5e", "Sending buy request to " + targetGm.name, packet);
             game.socket.emit(LootSheet5eNPC.SOCKET, packet);
@@ -602,7 +607,7 @@ class LootSheet5eNPC extends ActorSheet5eNPC {
         d.render(true);
     }
 
-    /* -------------------------------------------- */
+     /* -------------------------------------------- */
 
     /**
      * Handle Loot item
