8defa614c58a2a1ace3058683b4c8a4cbf114361
Daniel Böttner <jackprince1983@gmail.com>
2021-12-02 21:07:53 +0100

    better name for main helper class

--- modules/helper/LootSheetNPC5eHelper.js
+++ modules/helper/LootSheetNPC5eHelper.js
@@ -1,8 +1,6 @@
 import { PermissionHelper } from "./PermissionHelper.js";
 import { MODULE } from "../config.js";
 
-const ModulSocket = "module." + MODULE.ns;
-
 class LootSheetNPC5eHelper {
 
     /**
@@ -102,12 +100,12 @@ class LootSheetNPC5eHelper {
      * @param {*} options 
      * @returns {object}
      */
-    static _getOptionsDefault(options){
-         return {
+    static _getOptionsDefault(options) {
+        return {
             chanceOfDamagedItems: options ? options?.chanceOfDamagedItems | 0 : 0,
             damagedItemsMultiplier: options ? options?.damagedItemsMultiplier | 0 : 0,
             removeDamagedItems: options ? options?.removeDamagedItems | false : false
-            };
+        };
     }
 
     static _isItemDamaged(item, chanceOfDamagedItems) {
@@ -122,6 +120,7 @@ class LootSheetNPC5eHelper {
     }
 
     /* -------------------------------------------- */
+
     /**
      * Handle Loot item
      * @private
@@ -133,13 +132,14 @@ class LootSheetNPC5eHelper {
 
         if (!targetGm) return ui.notifications.error("No active GM on your scene, they must be online and on the same scene to purchase an item.");
         if (token === null) return ui.notifications.error(`You must loot items from a token.`);
-        if (!game.user.actorId) {
+        if ((!game.user.isGM && !game.user.actorId) || !game.user.character._id) {
             console.log("Loot Sheet | No active character for user");
             return ui.notifications.error(`No active character for user.`);
         }
 
         const itemId = event.currentTarget.dataset.itemId || event.currentTarget.closest('.item').dataset.itemId,
-            targetItem = token.actor.getEmbeddedEntity("Item", itemId);
+            targetItem = token.actor.getEmbeddedEntity("Item", itemId),
+            looterId = (game.user.isGM) ? targetGm.id : game.user.actorId;
 
         const item = { itemId: itemId, quantity: 1 };
         if (all || event.shiftKey) {
@@ -148,7 +148,7 @@ class LootSheetNPC5eHelper {
 
         const packet = {
             type: "loot",
-            looterId: game.user.actorId,
+            looterId: looterId,
             tokenId: token.id,
             items: [item],
             processorId: targetGm.id
@@ -156,14 +156,14 @@ class LootSheetNPC5eHelper {
 
         if (targetItem.data.data.quantity === item.quantity) {
             console.log("LootSheet5e", "Sending loot request to " + targetGm.name, packet);
-            game.socket.emit(ModulSocket, packet);
+            game.socket.emit(MODULE.socket, packet);
             return;
         }
 
         const dialog = new QuantityDialog((quantity) => {
             packet.items[0]['quantity'] = quantity;
             console.log("LootSheet5e", "Sending loot request to " + targetGm.name, packet);
-            game.socket.emit(ModulSocket, packet);
+            game.socket.emit(MODULE.socket, packet);
         },
             {
                 acceptLabel: "Loot"
@@ -172,8 +172,43 @@ class LootSheetNPC5eHelper {
         dialog.render(true);
     }
 
+    /**
+    * Handle Loot coins
+    * @private
+    */
+    static lootCoins(token, event) {
+        event.preventDefault();
+        if (!game.settings.get("lootsheetnpc5e", "lootCurrency")) {
+            return;
+        }
+        console.log("Loot Sheet | Loot Coins clicked");
+
+        const targetGm = PermissionHelper.getTargetGM();
+
+        if (!targetGm) {
+            return ui.notifications.error("No active GM on your scene, they must be online and on the same scene to loot coins.");
+        }
+
+        if (token === null) {
+            return ui.notifications.error(`You must loot coins from a token.`);
+        }
+        if (!game.user.actorId) {
+            console.log("Loot Sheet | No active character for user");
+            return ui.notifications.error(`No active character for user.`);
+        }
+
+        const packet = {
+            type: "lootCoins",
+            looterId: game.user.actorId,
+            tokenId: token.id,
+            processorId: targetGm.id
+        };
+        console.log("LootSheet5e", "Sending loot request to " + targetGm.name, packet);
+        game.socket.emit(MODULE.socket, packet);
+    }
+
     static distributeCoins(containerActor) {
-        let actorData = containerActor.data
+        let actorData = containerActor.data;
         let observers = [];
         let players = game.users.players;
 
@@ -266,69 +301,6 @@ class LootSheetNPC5eHelper {
                     content: message
                 });
             }
-        }
-    }
-
-    /**
-     * 
-     * @param {Actor5e} containerActor 
-     * @param {User} looter 
-     */
-    lootCoins(containerActor, looter) {
-        let actorData = containerActor.data
-
-        let sheetCurrency = LootSheetNPC5eHelper.convertCurrencyFromObject(actorData.data.currency);
-        //console.log("Loot Sheet | Currency data", currency);
-
-        // add currency to actors existing coins
-        let msg = [];
-        let currency = LootSheetNPC5eHelper.convertCurrencyFromObject(looter.data.data.currency),
-            newCurrency = duplicate(LootSheetNPC5eHelper.convertCurrencyFromObject(looter.data.data.currency));
-
-        //console.log("Loot Sheet | Current Currency", currency);
-
-        for (let c in currency) {
-            // add msg for chat description
-            if (sheetCurrency[c]) {
-                //console.log("Loot Sheet | New currency for " + c, currencySplit[c]);
-                msg.push(` ${sheetCurrency[c]} ${c} coins`)
-            }
-            if (sheetCurrency[c] != null) {
-                // Add currency to permitted actor
-                newCurrency[c] = parseInt(currency[c] || 0) + parseInt(sheetCurrency[c]);
-                looter.update({
-                    'data.currency': newCurrency
-                });
-            }
-        }
-
-        // Remove currency from loot actor.
-        let lootCurrency = LootSheetNPC5eHelper.convertCurrencyFromObject(containerActor.data.data.currency),
-            zeroCurrency = {};
-
-        for (let c in lootCurrency) {
-            zeroCurrency[c] = {
-                'type': sheetCurrency[c].type,
-                'label': sheetCurrency[c].type,
-                'value': 0
-            }
-            containerActor.update({
-                "data.currency": zeroCurrency
-            });
-        }
-
-        // Create chat message for coins received
-        if (msg.length != 0) {
-            let message = `${looter.data.name} receives: `;
-            message += msg.join(",");
-            ChatMessage.create({
-                user: game.user._id,
-                speaker: {
-                    actor: containerActor,
-                    alias: containerActor.name
-                },
-                content: message
-            });
         }
     }
 
