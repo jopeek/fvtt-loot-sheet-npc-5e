5de764b672d371d0f6c0fbaab7e85b3257ac719d
Jan Ole Peek <jopeek@gmail.com>
2021-12-21 09:33:24 -0800

    manual push to master because github is terrible

---
+++ test/test.js
@@ -0,0 +1,37 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import { LootSheetNPC5eHelper } from '../modules/helper/LootSheetNPC5eHelper.js';
+
+describe('Check module structure', function(){
+  it('Helpers exist', function(done) {
+    let directory = './modules/helper'; 
+    let dirs= fs.readdirSync(directory);
+    
+    dirs.map(function (filename) {
+      assert.strictEqual(filename, filename);        
+    });
+    done();
+  });
+});
+
+describe('Check Permissions', () => {
+  const pid = "aplayerID1234",
+        mockPlayer = { data: {_id: pid }},
+        mockActor = {permission: [{default: 0}]};
+        mockActor.permission[pid] = 2;
+
+        it(
+          'Get a players loot permission',
+          () => assert.strictEqual(LootSheetNPC5eHelper.getLootPermissionForPlayer(mockActor,mockPlayer),2)
+        );
+
+        it(
+          'Get default loot permissions',
+          () => {
+            mockPlayer.data._id = 'doesntexist';
+            assert.strictEqual(LootSheetNPC5eHelper.getLootPermissionForPlayer(mockActor,mockPlayer),0);
+          }
+        );
+
+       
+});
